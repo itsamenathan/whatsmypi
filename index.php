17830b6db9d8c234089f6c10e8b034940ae2c991
Nathan W <nathan@frcv.net>
2015-01-20 14:43:19 -0600

    cleaned up a bit

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
-  if( $_GET["ip"])
+  $file = 'ip.txt';
+  if( isset($_GET['ip']) )
   {
      $today = date("Y-m-d H:i:s");
      $ip = $_GET['ip'];
      if (filter_var($ip, FILTER_VALIDATE_IP)) {
-       $txt = sprintf("%s - %s", $today, $ip);
-       $myfile = fopen("ip.txt", "w") or die("Unable to open file!");
-       fwrite($myfile, $txt);
+       $line = sprintf("%s - %s", $today, $ip);
+       $content = file_get_contents($file);
+       $content = $line;
+       file_put_contents($file, $content);
      }
   }
-  $myfile = fopen("ip.txt", "r") or die("Unable to open file!");
-  echo fgets($myfile);
-  fclose($myfile);
+  if ( !isset($content) ) {
+    $content = file_get_contents($file);
+  }
+  echo $content;
 ?>
 
